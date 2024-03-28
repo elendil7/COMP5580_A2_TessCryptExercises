8f680c78a8e5c86202bbc7732f86381a464a63f6
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-03-28 01:58:44 +0000

    chore(ExerciseSolver): add methods for all other exercises (unsolved)

--- ExerciseSolver.java
+++ ExerciseSolver.java
@@ -77,3 +77,101 @@ public class ExerciseSolver extends FilesystemManager {
     setDecodedString(decoded);
   }
 
+  // * Exercise [#3] method
+  /*
+   * The plaintext
+   * comes from
+   * tess26.txt and
+   * is encoded
+   * with a
+   * Vigenere cipher.
+   * The key
+   * is an
+   * arbitrary sequence
+   * of six
+   * 
+   * letters (i.e. not necessarily forming an English
+   * word).
+   */
+  @BeforeSolving
+  @AfterSolving
+  public void exercise3() {
+    String decoded = new String(cexercise3);
+
+    // divide the text into 6 groups
+    String[] groups = CipherUtils.divideTextIntoBlocksOfNLength(decoded, 6);
+
+    // for each group, perform frequency analysis
+    ArrayList<FreqModel> freq = CipherUtils.analyzeCharacterFrequencies(groups);
+
+    // sort the frequencies
+    ArrayList<FreqModel> sortedFreq = CipherUtils.sortCharacterFrequencies(freq);
+
+    // print out the sorted frequencies
+    CipherUtils.printCharacterFrequencies(sortedFreq);
+  }
+
+  // * Exercise [#4] method
+  /*
+   * The plaintext
+   * comes from
+   * tess26.txt and
+   * is encoded
+   * with a
+   * Vigenere cipher.
+   * The key
+   * is an
+   * arbitrary sequence
+   * of between 4 and 6 letters.
+   */
+  @BeforeSolving
+  @AfterSolving
+  public void exercise4() {
+
+  }
+
+  // * Exercise [#5] method
+  /*
+   * The plaintext comes from tess26.txt and is encoded with a transposition
+   * cipher,
+   * as follows: the plaintext is written row-wise across a certain number of
+   * columns,
+   * between 4 and 6. (You must figure out how many columns were used.) The
+   * ciphertext is formed by reading out successive columns from left to right.
+   */
+  @BeforeSolving
+  @AfterSolving
+  public void exercise5() {
+  }
+
+  // * Exercise [#6] method
+  /*
+   * The plaintext comes from tess26.txt and is encoded with a transposition
+   * cipher,
+   * as follows: the plaintext is written row-wise across six columns. The
+   * ciphertext is
+   * formed by reading out successive columns in an arbitrary order (which you
+   * must
+   * figure out to decipher the message). Hint:look for common pairs of letters,
+   * such as
+   * 'th'.
+   */
+  @BeforeSolving
+  @AfterSolving
+  public void exercise6() {
+  }
+
+  // * Exercise [#7] method
+  /*
+   * The plaintext comes from tess27.txt and is encoded with a general
+   * substitution
+   * cipher, using a randomly chosen mapping from the 27-character alphabet onto
+   * itself. Note that normally (i.e. except by chance) a vertical bar will be
+   * mapped
+   * onto some other letter of the alphabet.
+   */
+  @BeforeSolving
+  @AfterSolving
+  public void exercise7() {
+  }
+}
