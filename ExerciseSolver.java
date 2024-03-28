996d43e88cf3ee25c53302b1cf5a5bc1593f9fe7
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-03-28 01:55:57 +0000

    feat(ExerciseSolver): load all file data into memory at runtime

---
+++ ExerciseSolver.java
@@ -0,0 +1,13 @@
+public class ExerciseSolver extends FilesystemManager {
+  // all data loaded from files
+  private String cexercise1 = getExerciseFileData("exercise1");
+  private String cexercise2 = getExerciseFileData("exercise2");
+  private String cexercise3 = getExerciseFileData("exercise3");
+  private String cexercise4 = getExerciseFileData("exercise4");
+  private String cexercise5 = getExerciseFileData("exercise5");
+  private String cexercise6 = getExerciseFileData("exercise6");
+  private String cexercise7 = getExerciseFileData("exercise7");
+  private String tess = getTextFileData("tess");
+  private String tess26 = getTextFileData("tess26");
+  private String tess27 = getTextFileData("tess27");
+
