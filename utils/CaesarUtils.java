289d6ea8069cb9a5abd81b2e564cf245744a6a17
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-03-30 00:27:01 +0000

    chore(Files): move some files around

---
+++ utils/CaesarUtils.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class CaesarUtils {
+  // caesar cipher rotations (we will shift by 1 each time)
+  public static String rotateCaesarCipher(String text, int shift) {
+    String result = "";
+    for (int i = 0; i < text.length(); i++) {
+      char c = text.charAt(i);
+      if (Character.isLetter(c)) {
+        if (Character.isUpperCase(c)) {
+          result += (char) ('A' + (c - 'A' + shift) % 26);
+        } else {
+          result += (char) ('a' + (c - 'a' + shift) % 26);
+        }
+      } else {
+        result += c;
+      }
+    }
+    return result;
+  }
+}
