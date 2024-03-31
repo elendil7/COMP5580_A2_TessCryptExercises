d82829621f20fe1acc557bf2ac74f187efc756bf
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-03-31 02:23:44 +0100

    feat(AppConfig): implement app configuration

---
+++ AppConfig.java
@@ -0,0 +1,25 @@
+public class AppConfig {
+  private static AppConfig instance = null;
+
+  public static boolean generalLoggingEnabled = true;
+  public static boolean cipherLoggingEnabled = true;
+  public static boolean plaintextLoggingEnabled = true;
+  public static boolean debugLoggingEnabled = false;
+
+  public static boolean runExercise1Enabled = true;
+  public static boolean runExercise2Enabled = true;
+  public static boolean runExercise3Enabled = true;
+  public static boolean runExercise4Enabled = true;
+  public static boolean runExercise5Enabled = true;
+  public static boolean runExercise6Enabled = true;
+  public static boolean runExercise7Enabled = false;
+
+  public static boolean solveOnlyForMe = false;
+
+  public static AppConfig getInstance() {
+    if (instance == null) {
+      instance = new AppConfig();
+    }
+    return instance;
+  }
+}
